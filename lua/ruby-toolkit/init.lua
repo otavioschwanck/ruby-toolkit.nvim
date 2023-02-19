ef0850c79d6eec5068f5bc43841beb22c039d606
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2023-02-19 14:20:42 +0000

    easier setup

---
+++ lua/ruby-toolkit/init.lua
@@ -0,0 +1,5 @@
+return {
+  extract_variable = require("ruby-toolkit.extract-variable").extract_variable,
+  extract_to_function = require("ruby-toolkit.extract-to-function").extract_to_function,
+  create_function_from_text = require("ruby-toolkit.create-function-from-text").create_function_from_text
+}
